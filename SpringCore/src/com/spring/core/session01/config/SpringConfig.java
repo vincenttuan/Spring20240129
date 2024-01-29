aa17db410870293964326c007bc0902034cf221e
vincenttuan <vincentjava@yahoo.com.tw>
2024-01-29 21:34:57 +0800

    java

--- SpringCore/src/com/spring/core/session01/config/SpringConfig.java
+++ SpringCore/src/com/spring/core/session01/config/SpringConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.spring.core.session01.bean.Hello;
+import com.spring.core.session01.bean.Lotto;
 
 @Configuration // Spring 配置檔
 public class SpringConfig {
@@ -12,6 +13,12 @@ public class SpringConfig {
 	public Hello getHello() {
 		Hello hello = new Hello();
 		return hello;
+	}
+	
+	@Bean(name = "lotto")
+	public Lotto getLotto() {
+		Lotto lotto = new Lotto();
+		return lotto;
 	}
 	
 }
