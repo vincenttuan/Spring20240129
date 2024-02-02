1fbed24b366d3d2404ba4d576a15aa71df29b3a1
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-02 21:26:18 +0800

    java

---
+++ SpringCore/src/com/spring/core/session02/bean/Color.java
@@ -0,0 +1,8 @@
+package com.spring.core.session02.bean;
+
+import lombok.Data;
+
+@Data
+public class Color {
+	private final String name;
+}
---
+++ SpringCore/src/com/spring/core/session02/bean/Size.java
@@ -0,0 +1,8 @@
+package com.spring.core.session02.bean;
+
+import lombok.Data;
+
+@Data
+public class Size {
+	private final String name;
+}
