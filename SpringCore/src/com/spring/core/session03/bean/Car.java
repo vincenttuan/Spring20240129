ebf9653cf3e2899bf4b40bb4af3590bd7ab46859
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-05 20:59:04 +0800

    java

---
+++ SpringCore/src/com/spring/core/session03/bean/Car.java
@@ -0,0 +1,13 @@
+package com.spring.core.session03.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+	private String name;
+	private Integer price;
+}
