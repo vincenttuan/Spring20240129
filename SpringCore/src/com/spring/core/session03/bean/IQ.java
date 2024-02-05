18e790289bb57b11b4cfbb9dc08bb0021f877f9d
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-05 20:00:44 +0800

    java

---
+++ SpringCore/src/com/spring/core/session03/bean/IQ.java
@@ -0,0 +1,13 @@
+package com.spring.core.session03.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IQ {
+	private String country; // 國家
+	private Integer score; // 分數
+}
