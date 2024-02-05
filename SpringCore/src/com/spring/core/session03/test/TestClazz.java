382c64b64967cd15049cf10ad6e76151ad885514
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-05 19:23:51 +0800

    java

---
+++ SpringCore/src/com/spring/core/session03/test/TestClazz.java
@@ -0,0 +1,11 @@
+package com.spring.core.session03.test;
+
+public class TestClazz {
+	public static void main(String[] args) {
+		// 計算出 beans-config3.xml 中 clazz1, clazz2, clazz3 的學分總和
+		// 並分別找出學分最高與最低的科目
+		
+		
+		
+	}
+}
