e61adc3b47e88c911a1d0470692c47017f206a99
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-05 21:33:51 +0800

    java

---
+++ SpringCore/src/com/spring/core/session03/bean/MySQL.java
@@ -0,0 +1,19 @@
+package com.spring.core.session03.bean;
+
+public class MySQL {
+	public void init() {
+		System.out.println("建立資料庫連線中...");
+		// ...
+		System.out.println("建立完成可以開始使用");
+	}
+	
+	public void query() {
+		System.out.println("資料查詢...");
+	}
+	
+	public void destory() {
+		System.out.println("資料庫連線關閉中");
+		// ...
+		System.out.println("關閉成功");
+	}
+}
