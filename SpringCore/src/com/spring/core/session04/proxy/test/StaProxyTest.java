0b9a43bdc4450bebda4e7756e5d250202fe51e20
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-19 19:18:15 +0800

    java

---
+++ SpringCore/src/com/spring/core/session04/proxy/test/StaProxyTest.java
@@ -0,0 +1,19 @@
+package com.spring.core.session04.proxy.test;
+
+import com.spring.core.session04.proxy.sta.Man;
+import com.spring.core.session04.proxy.sta.Person;
+import com.spring.core.session04.proxy.sta.Woman;
+
+public class StaProxyTest {
+
+	public static void main(String[] args) {
+		// 不透過代理直接執行
+		Person man = new Man();
+		Person woman = new Woman();
+		
+		man.work();
+		woman.work();
+
+	}
+
+}
