d8a94f7f3f3099bb3551632a605a4bf7b247f274
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-19 19:30:02 +0800

    java

---
+++ SpringCore/src/com/spring/core/session04/proxy/sta/PersonProxy.java
@@ -0,0 +1,23 @@
+package com.spring.core.session04.proxy.sta;
+
+// 靜態代理
+// 透過此類來執行代理任務
+public class PersonProxy implements Person {
+	
+	// 被代理的物件
+	private Person person;
+	
+	public PersonProxy(Person person) {
+		this.person = person;
+	}
+	
+	@Override
+	public void work() {
+		// 公用邏輯
+		System.out.println("戴口罩");
+		// 執行(被代理物件的)業務方法
+		person.work();
+	}
+	
+	
+}
