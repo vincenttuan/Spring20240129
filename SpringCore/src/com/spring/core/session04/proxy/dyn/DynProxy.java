56b1c3c3d3daf8162c8937edd8d03145b49b5967
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-19 21:00:14 +0800

    java

--- SpringCore/src/com/spring/core/session04/proxy/dyn/DynProxy.java
+++ SpringCore/src/com/spring/core/session04/proxy/dyn/DynProxy.java
@@ -3,6 +3,7 @@ package com.spring.core.session04.proxy.dyn;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 // 動態代理
 public class DynProxy {
@@ -23,6 +24,9 @@ public class DynProxy {
 		Class<?>[] interfaces = object.getClass().getInterfaces();
 		// 3. 處理代理的實現
 		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+			// Before:前置通知
+			System.out.printf("Before:前置通知 log: 方法名稱:%s 方法參數:%s%n", 
+					method.getName(), Arrays.toString(args));
 			Object resultObj = null;
 			// 業務邏輯-調用
 			resultObj = method.invoke(object, args); // 第一的參數要放的是被代理對象的物件
