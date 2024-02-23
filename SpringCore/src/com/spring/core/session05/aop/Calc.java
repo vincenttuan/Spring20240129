84d189ba7a23fd28f050bb46434973f59c111c71
vincenttuan <vincentjava@yahoo.com.tw>
2024-02-23 19:29:27 +0800

    java

---
+++ SpringCore/src/com/spring/core/session05/aop/Calc.java
@@ -0,0 +1,8 @@
+package com.spring.core.session05.aop;
+
+public interface Calc {
+	Integer add(Integer x, Integer y);
+	Integer sub(Integer x, Integer y);
+	Integer mul(Integer x, Integer y);
+	Integer div(Integer x, Integer y);
+}
