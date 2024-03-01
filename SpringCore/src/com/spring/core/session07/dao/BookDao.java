f8ef882f9f2f27578e67df114676605011bc855f
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-01 19:57:37 +0800

    java

---
+++ SpringCore/src/com/spring/core/session07/dao/BookDao.java
@@ -0,0 +1,13 @@
+package com.spring.core.session07.dao;
+
+public interface BookDao {
+	// 交易服務-查詢
+	Integer getBookPrice(Integer bookId); // 取得書本價格
+	Integer getBookStock(Integer bookId); // 取得書本庫存
+	Integer getWalletBalance(String username); // 取得該名客戶目前餘額
+	// 交易服務-更新
+	Integer reduceBookStock(Integer bookId, Integer amountToReduce); // 更新書本庫存(減量)
+	Integer reduceWalletBalance(String username, Integer bookPrice); // 更新錢包餘額(減量)
+	
+	
+}
