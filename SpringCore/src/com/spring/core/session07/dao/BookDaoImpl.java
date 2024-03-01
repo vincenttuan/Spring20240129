5136e7f7dd62d336a3e63d3011bdf5b4a16ff6c8
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-01 20:17:47 +0800

    java

--- SpringCore/src/com/spring/core/session07/dao/BookDaoImpl.java
+++ SpringCore/src/com/spring/core/session07/dao/BookDaoImpl.java
@@ -29,15 +29,37 @@ public class BookDaoImpl implements BookDao {
 	}
 
 	@Override
-	public Integer reduceBookStock(Integer bookId, Integer amountToReduce) {
-		// TODO Auto-generated method stub
-		return null;
+	public Integer reduceBookStock(Integer bookId, Integer amountToReduce) { // 更新書本庫存(減量)
+		// 1. 檢查庫存
+		Integer bookStock = getBookStock(bookId); // 取得該書庫存
+		if(bookStock < amountToReduce) { // 庫存不足
+			throw new RuntimeException(String.format("book_id: %d 庫存不足 (%d < %d)%n", bookId, bookStock, amountToReduce));
+		}
+		// 2. 修改庫存
+		String sql = "update stock set book_amount = book_amount - ? where book_id = ?";
+		return jdbcTemplate.update(sql, amountToReduce, bookId);
 	}
 
 	@Override
-	public Integer reduceWalletBalance(String username, Integer bookPrice) {
-		// TODO Auto-generated method stub
-		return null;
+	public Integer reduceWalletBalance(String username, Integer bookPrice) { // 更新錢包餘額(減量)
+		// 1. 檢查帳戶餘額
+		Integer balance = getWalletBalance(username); // 取得客戶目前餘額
+		if(balance < bookPrice) { // 餘額不足
+			throw new RuntimeException(String.format("username: %s 餘額不足 (%d < %d)%n", username, balance, bookPrice));
+		}
+		
+		// 2. 修改帳戶餘額
+		String sql = "update wallet set balance = balance - ? where username = ?";
+		return jdbcTemplate.update(sql, bookPrice, username);
 	}
 	
 }
+
+
+
+
+
+
+
+
+
