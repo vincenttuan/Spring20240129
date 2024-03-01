f6dc33c5f35ec922b636d054759e9188929d5fc8
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-01 18:50:27 +0800

    java

--- SpringCore/src/com/spring/core/session06/test/EmployeeTest.java
+++ SpringCore/src/com/spring/core/session06/test/EmployeeTest.java
@@ -1,14 +1,21 @@
 package com.spring.core.session06.test;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class EmployeeTest {
+import com.spring.core.session06.model.po.Employee;
+import com.spring.core.session06.service.EmployeeService;
 
+public class EmployeeTest {
+	
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		EmployeeService employeeService = ctx.getBean(EmployeeService.class);
 		
-
+		List<Employee> employees = employeeService.findAll();
+		employees.forEach(System.out::println);
 	}
 
 }
