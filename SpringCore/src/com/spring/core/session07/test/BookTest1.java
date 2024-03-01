23cdcb6f033675a9d1782320d941234145935b66
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-01 20:59:30 +0800

    java

--- SpringCore/src/com/spring/core/session07/test/BookTest1.java
+++ SpringCore/src/com/spring/core/session07/test/BookTest1.java
@@ -10,7 +10,7 @@ public class BookTest1 {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
 		BookOneService bookOneService = ctx.getBean(BookOneService.class);
 		// 買單本書
-		bookOneService.buyOne("Mary", 1);
+		bookOneService.buyOne("john", 1);
 		System.out.println("買單本書 OK");
 	}
 }
