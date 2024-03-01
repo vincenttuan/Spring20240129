4d1af5734d52326f77897ca19858b389998dd3d0
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-01 18:44:51 +0800

    java

---
+++ SpringCore/src/com/spring/core/session06/test/EmployeeTest.java
@@ -0,0 +1,14 @@
+package com.spring.core.session06.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		
+
+	}
+
+}
