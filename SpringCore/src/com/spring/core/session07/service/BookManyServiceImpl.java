9865e5124272d36cfe04e08c817009ba344d57bb
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-04 18:31:34 +0800

    java

--- SpringCore/src/com/spring/core/session07/service/BookManyServiceImpl.java
+++ SpringCore/src/com/spring/core/session07/service/BookManyServiceImpl.java
@@ -2,12 +2,14 @@ package com.spring.core.session07.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookManyServiceImpl implements BookManyService {
 	@Autowired
 	private BookOneService bookOneService;
-
+	
+	@Transactional
 	@Override
 	public void buyMany(String username, Integer... bookIds) {
 		// 要買很多本, 一本一本買
