d8ccc4b7f35fd99b781f4806f3d70ab2ec61b234
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-08 21:00:44 +0800

    java

--- Springboot-Web/src/main/java/com/example/demo/controller/DataController.java
+++ Springboot-Web/src/main/java/com/example/demo/controller/DataController.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.response.ApiResponse;
@@ -25,6 +26,20 @@ public class DataController {
 	public ResponseEntity<ApiResponse<Integer>> lotto() {
 		Integer lottoNumber = new Random().nextInt(100);
 		ApiResponse<Integer> apiResponse = new ApiResponse<>(true, "成功", lottoNumber);
+		return ResponseEntity.ok(apiResponse);
+	}
+	
+	@GetMapping("/pass")
+	public ResponseEntity<ApiResponse<Integer>> pass(@RequestParam("score") Integer score) {
+		ApiResponse<Integer> apiResponse = new ApiResponse<>();
+		apiResponse.setData(score);
+		if(score >= 60) {
+			apiResponse.setStatus(true);
+			apiResponse.setMessage("及格");
+		} else {
+			apiResponse.setStatus(false);
+			apiResponse.setMessage("不及格");
+		}
 		return ResponseEntity.ok(apiResponse);
 	}
 	
