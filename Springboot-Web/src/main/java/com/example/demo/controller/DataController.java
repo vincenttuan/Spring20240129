97a11d8589f8a485be35ecdf70f7d62ea44fb930
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-08 20:55:24 +0800

    java

--- Springboot-Web/src/main/java/com/example/demo/controller/DataController.java
+++ Springboot-Web/src/main/java/com/example/demo/controller/DataController.java
@@ -3,22 +3,29 @@ package com.example.demo.controller;
 import java.util.Date;
 import java.util.Random;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.response.ApiResponse;
 
 @RestController
 @RequestMapping("/data")
 public class DataController {
 	
 	@GetMapping("/today")
-	public Date today() {
-		return new Date();
+	public ResponseEntity<ApiResponse<Date>> today() {
+		Date today = new Date();
+		ApiResponse<Date> apiResponse = new ApiResponse<>(true, "成功", today);
+		return ResponseEntity.ok(apiResponse);
 	}
 	
 	@GetMapping("/lotto")
-	public Integer lotto() {
-		return new Random().nextInt(100);
+	public ResponseEntity<ApiResponse<Integer>> lotto() {
+		Integer lottoNumber = new Random().nextInt(100);
+		ApiResponse<Integer> apiResponse = new ApiResponse<>(true, "成功", lottoNumber);
+		return ResponseEntity.ok(apiResponse);
 	}
 	
 }
