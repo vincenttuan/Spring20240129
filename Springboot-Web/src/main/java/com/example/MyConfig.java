a3974f8c1664601aef0e76c6765033d7eb78b8d7
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-11 21:29:29 +0800

    java

---
+++ Springboot-Web/src/main/java/com/example/MyConfig.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class MyConfig {
+	// 註冊 ModelMapper
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+	// 過濾 http 方法
+	// 可以將帶有 _method="PUT" 的 post 表單轉 PUT 送出
+	// 可以將帶有 _method="DELETE" 的 post 表單轉 DELETE 送出
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+}
