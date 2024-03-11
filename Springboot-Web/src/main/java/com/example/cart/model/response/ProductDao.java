3d3b78381a8fae2a7fe890a21b5ec304253ca13d
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-11 19:42:39 +0800

    java

---
+++ Springboot-Web/src/main/java/com/example/cart/model/response/ProductDao.java
@@ -0,0 +1,18 @@
+package com.example.cart.model.response;
+
+import java.util.List;
+
+import com.example.cart.model.po.Product;
+
+public interface ProductDao {
+	
+	List<Product> queryAllProducts();
+	Product getProductById(Integer id);
+	Product addProduct(Product product);
+	Product updateProduct(Product product);
+	Product updateProductQty(Integer id, Integer qty); // 修改庫存(庫存覆蓋)
+	Product addProductQty(Integer id, Integer increment); // 增量庫存
+	Product reduceProductQty(Integer id, Integer decrement); // 減量庫存
+	Boolean deleteProductById(Integer id);
+	
+}
