ef8c8d84cad591e6903daf6ff28b489a92c7ba06
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-11 20:53:05 +0800

    java

---
+++ Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.cart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+	
+	 
+	
+}
--- Springboot-Web/src/main/java/com/example/cart/model/dto/ProductDto.java
+++ Springboot-Web/src/main/java/com/example/cart/model/dto/ProductDto.java
@@ -1,7 +1,5 @@
 package com.example.cart.model.dto;
 
-import com.example.cart.model.po.Product;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +10,7 @@ import lombok.NoArgsConstructor;
 public class ProductDto {
 	private Integer id;    // 商品 id
 	private String name;   // 商品名稱
+	private Integer cost;  // 商品成本
 	private Integer price; // 商品售價
 	private Integer qty;   // 商品數量
 }
