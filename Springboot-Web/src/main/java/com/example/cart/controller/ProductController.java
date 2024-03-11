295de58b6fee06f8cfbb3d72df62f976f9f9a8b4
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-11 21:42:56 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
+++ Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
@@ -8,10 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.cart.model.dto.ProductDto;
 import com.example.cart.model.po.Product;
+import com.example.cart.model.response.StatusMessage;
 import com.example.cart.service.ProductService;
 import com.example.demo.model.response.ApiResponse;
 
@@ -25,7 +28,7 @@ public class ProductController {
 	@GetMapping
 	public ResponseEntity<ApiResponse<List<ProductDto>>> queryAllProducts() {
 		List<ProductDto> productDtos = productService.queryAllProducts();
-		ApiResponse<List<ProductDto>> apiResponse = new ApiResponse<>(true, "成功", productDtos);
+		ApiResponse<List<ProductDto>> apiResponse = new ApiResponse<>(true, StatusMessage.成功.name(), productDtos);
 		return ResponseEntity.ok(apiResponse);
 	}
 	
@@ -34,10 +37,17 @@ public class ProductController {
 		ProductDto productDto = productService.getProductById(id);
 		ApiResponse<ProductDto> apiResponse = null;
 		if (productDto == null) {
-			apiResponse = new ApiResponse<>(false, "查無資料", null);
+			apiResponse = new ApiResponse<>(false, StatusMessage.查無資料.name(), null);
 		} else {
-			apiResponse = new ApiResponse<>(true, "成功", productDto);
+			apiResponse = new ApiResponse<>(true, StatusMessage.成功.name(), productDto);
 		}
+		return ResponseEntity.ok(apiResponse);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ApiResponse<ProductDto>> addProduct(@RequestBody ProductDto productDto) {
+		ProductDto savedProductDto = productService.addProduct(productDto);
+		ApiResponse<ProductDto> apiResponse = new ApiResponse<>(true, StatusMessage.新增成功.name(), savedProductDto);
 		return ResponseEntity.ok(apiResponse);
 	}
 }
---
+++ Springboot-Web/src/main/java/com/example/cart/model/response/StatusMessage.java
@@ -0,0 +1,5 @@
+package com.example.cart.model.response;
+
+public enum StatusMessage {
+	查無資料, 成功, 新增成功
+}
