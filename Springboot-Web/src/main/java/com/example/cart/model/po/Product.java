6d098a8d1b92ea9ac33369325181f4a6a70036d2
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-11 19:31:46 +0800

    java

---
+++ Springboot-Web/src/main/java/com/example/cart/model/po/Product.java
@@ -0,0 +1,16 @@
+package com.example.cart.model.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	private Integer id;    // 商品 id
+	private String name;   // 商品名稱
+	private Integer cost;  // 商品成本
+	private Integer price; // 商品售價
+	private Integer qty;   // 商品數量
+}
