2bf72b0719a4c987b0b4025ea4c9820e5afbe6bf
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-11 21:14:25 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
+++ Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
@@ -1,13 +1,31 @@
 package com.example.cart.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.cart.model.dto.ProductDto;
+import com.example.cart.model.po.Product;
+import com.example.cart.service.ProductService;
+import com.example.demo.model.response.ApiResponse;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 	
-	 
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<ProductDto>>> queryAllProducts() {
+		List<ProductDto> productDtos = productService.queryAllProducts();
+		ApiResponse<List<ProductDto>> apiResponse = new ApiResponse<>(true, "成功", productDtos);
+		return ResponseEntity.ok(apiResponse);
+	}
 	
 }
