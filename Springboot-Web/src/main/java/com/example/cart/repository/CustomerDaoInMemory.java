cbd29a034f419baefe1fbf0895899f582921afac
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-15 20:56:07 +0800

    java

---
+++ Springboot-Web/src/main/java/com/example/cart/repository/CustomerDaoInMemory.java
@@ -0,0 +1,51 @@
+package com.example.cart.repository;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.cart.model.po.Customer;
+import com.example.cart.model.po.Product;
+
+@Repository("InMemory")
+public class CustomerDaoInMemory implements CustomerDao {
+	
+	private static List<Customer> customers = new CopyOnWriteArrayList<>();
+	
+	static {
+		customers.add(new Customer(1, "John", "1234"));
+		customers.add(new Customer(2, "Mary", "5678"));
+		customers.add(new Customer(3, "Jack", "1111"));
+		customers.add(new Customer(4, "Rose", "2222"));
+	}
+	
+	@Override
+	public List<Customer> findAllCustomers() {
+		return customers;
+	}
+
+	@Override
+	public Customer getCustomerById(Integer id) {
+		return customers.stream().filter(customer-> customer.getId().equals(id)).findFirst().orElseGet(null);
+	}
+
+	@Override
+	public Customer addCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean deleteCustomerById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
