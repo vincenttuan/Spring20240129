ce4575bb9c73e318a854d42b943c91ceec606a0f
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-15 21:11:00 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/service/CustomerService.java
+++ Springboot-Web/src/main/java/com/example/cart/service/CustomerService.java
@@ -2,9 +2,13 @@ package com.example.cart.service;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.example.cart.model.dto.CustomerDto;
+import com.example.cart.model.dto.ProductDto;
 import com.example.cart.model.po.Customer;
 import com.example.cart.repository.CustomerDao;
 
@@ -12,23 +16,36 @@ import com.example.cart.repository.CustomerDao;
 public class CustomerService {
 	
 	@Autowired
+	@Qualifier("InMemory")
 	private CustomerDao customerDao;
 	
-	public List<Customer> findAllCustomers() {
-		return customerDao.findAllCustomers();
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public List<CustomerDto> findAllCustomers() {
+		List<Customer> customers = customerDao.findAllCustomers();
+		// PO 轉 DTO
+		return customers.stream()
+				.map(customer -> modelMapper.map(customer, CustomerDto.class))
+				.toList();
 	}
 	
-	public Customer getCustomerById(Integer id) {
-		return customerDao.getCustomerById(id);
+	public CustomerDto getCustomerById(Integer id) {
+		Customer customer = customerDao.getCustomerById(id);
+		return modelMapper.map(customer, CustomerDto.class);
 	}
 	
-	public Customer addCustomer(Customer customer) {
-		return customerDao.addCustomer(customer);
+	public CustomerDto addCustomer(CustomerDto customerDto) {
+		Customer customer = modelMapper.map(customerDto, Customer.class);
+		Customer savedCustomer = customerDao.addCustomer(customer);
+		return modelMapper.map(savedCustomer, CustomerDto.class);
 	}
 	
-	public Customer updateCustomer(Integer id, Customer customer) {
-		customer.setId(id);
-		return customerDao.updateCustomer(customer);
+	public CustomerDto updateCustomer(Integer id, CustomerDto customerDto) {
+		customerDto.setId(id);
+		Customer customer = modelMapper.map(customerDto, Customer.class);
+		Customer updatedCustomer = customerDao.updateCustomer(customer);
+		return modelMapper.map(updatedCustomer, CustomerDto.class);
 	}
 	
 	public Boolean deleteCustomer(Integer id) {
