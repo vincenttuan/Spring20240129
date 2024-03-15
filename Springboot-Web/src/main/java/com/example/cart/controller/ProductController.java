4dc60700b9342065d89b4b1d4d59d77024e85c6e
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-15 19:29:04 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
+++ Springboot-Web/src/main/java/com/example/cart/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -50,4 +51,14 @@ public class ProductController {
 		ApiResponse<ProductDto> apiResponse = new ApiResponse<>(true, StatusMessage.新增成功.name(), savedProductDto);
 		return ResponseEntity.ok(apiResponse);
 	}
+	
+	@PutMapping("/{productId}")
+	public ResponseEntity<ApiResponse<ProductDto>> updateProduct(@PathVariable("productId") Integer productId,
+																 ProductDto productDto) {
+		ProductDto updatedProductDto = productService.updateProduct(productId, productDto);
+		ApiResponse<ProductDto> apiResponse = new ApiResponse<>(true, StatusMessage.修改成功.name(), updatedProductDto);
+		return ResponseEntity.ok(apiResponse);
+	}
+	
+	
 }
--- Springboot-Web/src/main/java/com/example/cart/model/response/StatusMessage.java
+++ Springboot-Web/src/main/java/com/example/cart/model/response/StatusMessage.java
@@ -1,5 +1,5 @@
 package com.example.cart.model.response;
 
 public enum StatusMessage {
-	查無資料, 成功, 新增成功
+	查無資料, 成功, 新增成功, 修改成功
 }
