e52282a807204ef2e8d73c2778bf2ee397c71cf5
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-15 21:39:57 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/model/dto/OrderDto.java
+++ Springboot-Web/src/main/java/com/example/cart/model/dto/OrderDto.java
@@ -1,5 +1,7 @@
 package com.example.cart.model.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,6 @@ import lombok.NoArgsConstructor;
 public class OrderDto {
 	private Integer id; // 訂單 id
 	private String date; // 訂單日期
-	private Integer customerId; // 客戶編號
+	private CustomerDto customerDto; // 客戶物件
+	private List<ItemDto> itemDtos; // 細目列表
 }
