3a03d1310d300f705907d04d85e6dd1464ef68d0
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-18 21:02:59 +0800

    java

---
+++ Springboot-Web/src/main/resources/static/js/cart.js
@@ -0,0 +1,12 @@
+
+const fetchUsername = async() => {
+	const response = await fetch('http://localhost:8080/customer/username');
+	if(response.ok) {
+		console.log(response.json);	
+	}
+};
+
+// 等待 DOM 加載完畢後再執行
+document.addEventListener("DOMContentLoaded", async() => {
+	fetchUsername(); // 取得 username
+});
