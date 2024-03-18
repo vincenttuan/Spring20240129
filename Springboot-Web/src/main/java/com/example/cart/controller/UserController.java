354b7eb22c566e58aa6736a0960b549deaa87c82
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-18 21:06:57 +0800

    java

---
+++ Springboot-Web/src/main/java/com/example/cart/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.cart.controller;
+
+import java.security.Principal;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.cart.model.response.StatusMessage;
+import com.example.demo.model.response.ApiResponse;
+
+@Controller
+@RequestMapping("/username")
+public class UserController {
+	
+	@GetMapping
+	public ResponseEntity<ApiResponse<String>> getLoginUsername(Principal principal) {
+		String username = principal.getName(); // 取得登入者的名字
+		ApiResponse<String> apiResponse = new ApiResponse<>(true, StatusMessage.成功.name(), username);
+		return ResponseEntity.ok(apiResponse);
+	}
+		
+}
