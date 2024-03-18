ef71546b54f1f72751cd8248918f505217d97d34
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-18 21:45:32 +0800

    java

--- Springboot-Web/src/main/resources/static/js/cart.js
+++ Springboot-Web/src/main/resources/static/js/cart.js
@@ -10,7 +10,7 @@ const fetchUsername = async() => {
 		console.log(JSON.parse(data));
 		console.log(JSON.parse(data).username);
 		
-		document.getElementById("username").innerHTML = JSON.parse(data).username;
+		$("username").innerHTML = JSON.parse(data).username;
 	}
 };
 
@@ -19,12 +19,16 @@ const logout = () => window.location.href = '/logout';
 const loadHTML = async(filePath, containerId) => {
 	const response = await fetch(filePath); // 抓取網頁
 	const data = await response.text(); // 網頁內容
-	document.getElementById(containerId).innerHTML = data; // 將網頁內容放到指定容器中
+	$(containerId).innerHTML = data; // 將網頁內容放到指定容器中
+};
+
+const $ = (id) => {
+	return document.getElementById(id);
 };
 
 // 等待 DOM 加載完畢後再執行
 document.addEventListener("DOMContentLoaded", async() => {
-	document.getElementById("logout").addEventListener("click", logout);
+	$("logout").addEventListener("click", logout);
 	
 	fetchUsername(); // 取得 username
 	await loadHTML('products-list.html', 'products-container');
