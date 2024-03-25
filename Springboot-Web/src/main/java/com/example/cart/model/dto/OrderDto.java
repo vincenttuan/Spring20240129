b50371fa500280334d29bf33839fff12eff338a6
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-25 19:38:52 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/model/dto/OrderDto.java
+++ Springboot-Web/src/main/java/com/example/cart/model/dto/OrderDto.java
@@ -12,6 +12,8 @@ import lombok.NoArgsConstructor;
 public class OrderDto {
 	private Integer id; // 訂單 id
 	private String date; // 訂單日期
+	private Boolean updatable; // 是否可以更新訂單(若日期小於今日則不可以修改)
+	private Integer total; // 訂單總金額
 	private CustomerDto customerDto; // 客戶物件
 	private List<ItemDto> itemDtos; // 細目列表
 }
