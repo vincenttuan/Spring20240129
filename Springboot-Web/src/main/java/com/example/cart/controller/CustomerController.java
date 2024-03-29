2aea688cc7b26f1a574941120fd5ca61cb6ac045
vincenttuan <vincentjava@yahoo.com.tw>
2024-03-29 20:36:41 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/controller/CustomerController.java
+++ Springboot-Web/src/main/java/com/example/cart/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -66,6 +67,16 @@ public class CustomerController {
 		Boolean status = customerService.deleteCustomerById(customerId);
 		ApiResponse<Boolean> apiResponse = new ApiResponse<>(status, 
 				status ? StatusMessage.刪除成功.name() : StatusMessage.刪除失敗.name(), 
+				status);
+		return ResponseEntity.ok(apiResponse);
+	}
+	
+	@PatchMapping("/{customerId}/update-password")
+	public ResponseEntity<ApiResponse<Boolean>> updatePassword(@PathVariable("customerId") Integer customerId, @RequestBody CustomerDto customerDto) {
+		String newPassword = customerDto.getPassword();
+		Boolean status = customerService.updatePassword(customerId, newPassword);
+		ApiResponse<Boolean> apiResponse = new ApiResponse<>(status, 
+				status ? StatusMessage.修改成功.name() : StatusMessage.修改失敗.name(), 
 				status);
 		return ResponseEntity.ok(apiResponse);
 	}
