47a0834f5ef0e6ad10126e7fcc71f123521a6f87
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-01 19:43:07 +0800

    java

--- Springboot-Web/src/main/java/com/example/cart/repository/ProductDaoInMySQL.java
+++ Springboot-Web/src/main/java/com/example/cart/repository/ProductDaoInMySQL.java
@@ -2,6 +2,10 @@ package com.example.cart.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.example.cart.model.po.Product;
@@ -9,16 +13,27 @@ import com.example.cart.model.po.Product;
 @Repository("InMySQLproduct")
 public class ProductDaoInMySQL implements ProductDao {
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	private RowMapper<Product> rowMapper = new BeanPropertyRowMapper<>(Product.class);
+	
 	@Override
 	public List<Product> queryAllProducts() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, name, cost, price, qty from product order by id";
+		return jdbcTemplate.query(sql, rowMapper);
 	}
 
 	@Override
 	public Product getProductById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select id, name, cost, price, qty from product where id = ?";
+		try {
+			Product product = jdbcTemplate.queryForObject(sql, rowMapper, id);
+			return product;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override
