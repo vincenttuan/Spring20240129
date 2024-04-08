b4b85d73264574ee02cbc99de42d684804d0854d
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-08 20:28:50 +0800

    java

--- Springboot-MQ/src/main/java/com/example/demo/mq/Main.java
+++ Springboot-MQ/src/main/java/com/example/demo/mq/Main.java
@@ -13,18 +13,18 @@ public class Main {
 		new Thread(consumer).start();
 		
 		// 啟動生產者
+		Scanner scanner = new Scanner(System.in);
 		while (producer.getCoffeeStock() > 0) {
 			// 設定訊息內容
-			Scanner scanner = new Scanner(System.in);
 			System.out.print("請設定咖啡名稱(訊息內容):");
 			String message = scanner.next();
 			producer.setMessage(message);
-			scanner.close();
 			
 			// 啟動執行緒生產咖啡(發布訊息)
 			new Thread(producer).start();
 			//Thread.sleep(1000); // delay 1 秒
 		}
+		scanner.close();
 		
 		System.out.println("程式結束");
 		System.exit(0);
