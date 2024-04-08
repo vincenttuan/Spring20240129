08663f7a1ddeb6b49aa9153addd773c6fbc3ad23
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-08 20:26:08 +0800

    java

--- Springboot-MQ/src/main/java/com/example/demo/mq/Producer.java
+++ Springboot-MQ/src/main/java/com/example/demo/mq/Producer.java
@@ -8,6 +8,9 @@ public class Producer implements Runnable {
 	// 咖啡庫存
 	private int coffeeStock;
 	
+	// 訊息內容
+	private String message;
+	
 	public Producer(MessageQueue queue, int coffeeStock) {
 		this.queue = queue;
 		this.coffeeStock = coffeeStock;
@@ -17,10 +20,14 @@ public class Producer implements Runnable {
 	public void run() {
 		System.out.println("Producer 生產者執行...");
 		System.out.println("目前咖啡庫存: " + coffeeStock);
-		// 模擬生產一杯拿鐵咖啡
-		queue.produce("拿鐵");
+		// 模擬生產一杯 XX 咖啡
+		queue.produce(message);
 		// 更新庫存
 		coffeeStock--;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
 	}
 	
 	public int getCoffeeStock() {
