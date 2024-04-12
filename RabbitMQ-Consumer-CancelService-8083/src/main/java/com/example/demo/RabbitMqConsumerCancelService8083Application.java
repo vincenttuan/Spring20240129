2afd10c9c684700270daec0937fad95da223b324
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-12 19:43:11 +0800

    java

---
+++ RabbitMQ-Consumer-CancelService-8083/src/main/java/com/example/demo/RabbitMqConsumerCancelService8083Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConsumerCancelService8083Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqConsumerCancelService8083Application.class, args);
+	}
+
+}
