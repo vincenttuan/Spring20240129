0065236607734d13eedabd022fecb263f11ad7d9
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-12 20:04:55 +0800

    java

--- RabbitMQ-Producer-Service-8081/src/main/java/com/example/demo/controller/ApiController.java
+++ RabbitMQ-Producer-Service-8081/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,16 @@ public class ApiController {
 		rabbitTemplate.convertAndSend(exchangeName, routingKey, data);
 		return "Ordered a coffee: " + coffeeOrder;
 	}
+	
+	@GetMapping("/cancel/{orderId}")
+	public String cancelOrder(@PathVariable Integer orderId) {
+		String data = String.format("{\"orderId\": %d}", orderId);
+		String routingKey = "coffee-cancel";
+		String exchangeName = "coffee-exchange";
+		// 將資料傳送給 MQ
+		rabbitTemplate.convertAndSend(exchangeName, routingKey, data);
+		return "Canceled a coffee order: " + data;
+	}
+	
 	
 }
