f094a3353a6d4f101b00b0bad60ce95f5864a2c1
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-12 21:11:21 +0800

    java

--- RabbitMQ-Consumer-OrderService-8082/src/main/java/com/example/demo/service/CoffeeOrderService.java
+++ RabbitMQ-Consumer-OrderService-8082/src/main/java/com/example/demo/service/CoffeeOrderService.java
@@ -1,8 +1,16 @@
 package com.example.demo.service;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CoffeeOrderService {
+	
+	// 監聽 RabbitMQ
+	@RabbitListener(queues = {"coffee-order-queue"})
+	public void consumeOrderMessageFromQueue(String coffeeOrder) {
+		System.out.println("Consumed a coffee order: " + coffeeOrder);
+		System.out.println("接到了 coffee 訂單資料: " + coffeeOrder);
+	}
 	
 }
