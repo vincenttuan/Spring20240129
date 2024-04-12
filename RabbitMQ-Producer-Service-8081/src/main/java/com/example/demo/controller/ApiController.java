7591eb1eca65b332fcea871dc63fb96de1568e7f
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-12 19:56:55 +0800

    java

---
+++ RabbitMQ-Producer-Service-8081/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@PostMapping("/order")
+	public String order(@RequestBody String coffeeOrder) {
+		String data = coffeeOrder; // 要送給 MQ 的內容 
+		String routingKey = "coffee-order";
+		String exchangeName = "coffee-exchange";
+		rabbitTemplate.convertAndSend(exchangeName, routingKey, data);
+		return "Ordered a coffee: " + coffeeOrder;
+	}
+	
+}
