1b3c0d59a221ed5ab4685d931cb5e8bd70bf14cf
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-15 19:55:12 +0800

    java

--- RabbitMQ-Consumer-OrderService-8082/src/main/java/com/example/demo/service/CoffeeOrderService.java
+++ RabbitMQ-Consumer-OrderService-8082/src/main/java/com/example/demo/service/CoffeeOrderService.java
@@ -7,19 +7,27 @@ import org.springframework.stereotype.Service;
 public class CoffeeOrderService {
 	
 	// 處理特價廣播
+	
+	// log1 與 log2 會輪流接收
 	@RabbitListener(queues = {"logQueue"})
-	public void receiveOnSaleBroadcast(String message) {
+	public void receiveLog1(String message) {
 		//System.out.println("統一收到特價廣播: " + message);
-		System.out.println("寫 log: " + message);
+		System.out.println("寫 log1: " + message);
+	}
+	
+	@RabbitListener(queues = {"logQueue"})
+	public void receiveLog2(String message) {
+		//System.out.println("統一收到特價廣播: " + message);
+		System.out.println("寫 log2: " + message);
 	}
 	
 	@RabbitListener(queues = {"staffQueue"})
-	public void receiveOnSaleBroadcast2(String message) {
+	public void receiveOnSaleToStaff(String message) {
 		System.out.println("staff 收到特價廣播發 email: " + message);
 	}
 	
 	@RabbitListener(queues = {"customerQueue"})
-	public void receiveOnSaleBroadcast3(String message) {
+	public void receiveOnSaleToCustomer(String message) {
 		System.out.println("customer 收到特價廣播發 sms: " + message);
 	}
 	
