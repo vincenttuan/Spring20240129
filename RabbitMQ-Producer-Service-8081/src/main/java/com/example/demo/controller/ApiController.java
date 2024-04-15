bde3ed0d8381ce48c12afbd8123b81bdfeceedfa
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-15 21:39:11 +0800

    java

--- RabbitMQ-Producer-Service-8081/src/main/java/com/example/demo/controller/ApiController.java
+++ RabbitMQ-Producer-Service-8081/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,9 +61,22 @@ public class ApiController {
 	}
 	
 	// 測試方法: http://localhost:8081/api/iot/send
-    // json1: {"queueName":"temperature-data-queue","data":"25.5 C","headers":{"data-type":"temperature", "unit":"celsius"}}
-    // json2: {"queueName":"humidity-data-queue","data":"80%","headers":{"data-type":"humidity", "unit":"percentage"}}
-    
+    // json1: {"queueName":"temperature-data-queue","data":"25.5","headers":{"data-type":"temperature", "unit":"celsius"}}
+    // json2: {"queueName":"humidity-data-queue","data":"80","headers":{"data-type":"humidity", "unit":"percentage"}}
+    @PostMapping("/iot/send")
+    public String sendIOTMessage(@RequestBody Map<String, Object> messageData) {
+    	String queueName = (String)messageData.get("queueName");
+    	String data = (String)messageData.get("data");
+    	Map<String, Object> headers = (Map<String, Object>)messageData.get("headers");
+    	
+    	// 將 headers 放到 AMQP Message 的標頭中
+    	rabbitTemplate.convertAndSend("iot-data-exchange", "", data, (Message message) -> {
+    		headers.forEach((key , value) -> message.getMessageProperties().setHeader(key, value));
+    		return message;
+    	});
+    	
+    	return "Send iot message: " + messageData;
+    }
 	
 	
 }
