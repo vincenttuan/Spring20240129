5cfaa1bbc306f8ca10fb62c58d960e5f71e32f4e
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-15 21:09:13 +0800

    java

--- RabbitMQ-Consumer-OrderService-8082/src/main/java/com/example/demo/config/RabbitMQTopicsConfig.java
+++ RabbitMQ-Consumer-OrderService-8082/src/main/java/com/example/demo/config/RabbitMQTopicsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,14 +30,16 @@ public class RabbitMQTopicsConfig {
 	@Bean
 	// "news.sports.*" `.*` 更加嚴格，僅匹配一個點劃分的單詞。
     // 例如: news.sports.football, news.sports.basketball
-	Binding bindingSportsNewsQueue(Queue sportsNewsQueue, TopicExchange newsExchange) {
+	Binding bindingSportsNewsQueue(@Qualifier("soprtsNewsQueue") Queue sportsNewsQueue, 
+								   @Qualifier("newsExchange") TopicExchange newsExchange) {
 		return BindingBuilder.bind(sportsNewsQueue).to(newsExchange).with("news.sports.*");
 	}
 	
 	@Bean
 	// "news.tech.#" `.#` 更加靈活，可以匹配任意數量的單詞，包括零個。
     // 例如: news.tech, news.tech.python, news.tech.java.spring
-	Binding bindingTechNewsQueue(Queue techNewsQueue, TopicExchange newsExchange) {
+	Binding bindingTechNewsQueue(@Qualifier("techNewsQueue") Queue techNewsQueue, 
+								 @Qualifier("newsExchange") TopicExchange newsExchange) {
 		return BindingBuilder.bind(techNewsQueue).to(newsExchange).with("news.tech.#");
 	}
 	
