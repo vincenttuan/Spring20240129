550b06d5be61aec60d4ce808f175def77d676016
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-19 18:51:27 +0800

    java

---
+++ Springboot-JPA-Bento-8084/src/test/java/com/example/demo/SpringbootJpaBento8084ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaBento8084ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
