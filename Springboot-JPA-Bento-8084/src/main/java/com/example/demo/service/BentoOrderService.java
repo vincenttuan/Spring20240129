8acd632a4e36e2945853db58e4ddfb641b6dcf89
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-19 20:58:20 +0800

    java

---
+++ Springboot-JPA-Bento-8084/src/main/java/com/example/demo/service/BentoOrderService.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Bento;
+import com.example.demo.model.BentoOrder;
+import com.example.demo.repository.BentoOrderRepository;
+import com.example.demo.repository.BentoRepository;
+
+@Service
+public class BentoOrderService {
+	
+	@Autowired
+	private BentoOrderRepository bentoOrderRepository;
+	
+	@Autowired
+	private BentoService bentoService;
+	
+	public BentoOrder createBentoOrder(BentoOrder bentoOrder) {
+		Long bentoId = bentoOrder.getBento().getId();
+		// 確認是否有此便當
+		Bento bento = bentoService.getBentoById(bentoId);
+		if(bento == null) {
+			throw new RuntimeException("Bento 不存在");
+		}
+		// 確認庫存數量是否足夠
+		if(bento.getQuantity() < bentoOrder.getAmount()) {
+			throw new RuntimeException("Bento 庫存不足");
+		}
+		
+		// 下訂單
+		BentoOrder savedBentoOrder = bentoOrderRepository.save(bentoOrder);
+		// Bento 庫存要扣除
+		bentoService.decreaseBentoQuantity(bentoId, bentoOrder.getAmount());
+		
+		return bentoOrder;
+	}
+	
+	
+}
