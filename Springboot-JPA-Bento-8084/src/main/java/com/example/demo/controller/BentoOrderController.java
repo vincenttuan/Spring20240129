faa7f1c2c775922f537ce3a6b9aae476db0e344e
vincenttuan <vincentjava@yahoo.com.tw>
2024-04-19 21:17:24 +0800

    java

--- Springboot-JPA-Bento-8084/src/main/java/com/example/demo/controller/BentoOrderController.java
+++ Springboot-JPA-Bento-8084/src/main/java/com/example/demo/controller/BentoOrderController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +25,24 @@ public class BentoOrderController {
 	
 	@GetMapping
 	public List<BentoOrder> getBentoOrders() {
-		return bentoOrderService.
+		return bentoOrderService.getAllBentoOrders();
 	}
+	
+	@GetMapping("/{id}")
+	public BentoOrder getBentoOrder(@PathVariable Long id) {
+		return bentoOrderService.getBentoOrderById(id);
+	}
+	
+	@PostMapping
+	// json: {"bento": {"id":1}, "amount":2}
+	public BentoOrder addBentoOrder(@RequestBody BentoOrder bentoOrder) {
+		return bentoOrderService.createBentoOrder(bentoOrder);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteBentoOrder(@PathVariable Long id) {
+		bentoOrderService.deleteBentoOrder(id);
+	}
+	
 	
 }
